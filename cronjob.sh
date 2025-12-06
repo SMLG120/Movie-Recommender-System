c6b81d4f8ce545ac05f6667d494f0a0f588c32d7
jojo <jessica.ojo@mail.mcgill.ca>
2025-12-05 21:04:04 -0500

    minor fixes

--- cronjob.sh
+++ cronjob.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd91f14e526dd1ba710454dd6834e08e29fa7ce3ef267691d1bb14b0825cd72c
-size 1134
+oid sha256:59ec8b1efb9e4fdeaa1288206063174f0ef8350c998b4d74d5d0e2f574580b47
+size 1145
